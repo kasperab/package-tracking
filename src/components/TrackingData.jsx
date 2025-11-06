93166cbc7b5fc652a689e0946015c7b7ed070a6a
kasperab <kasperab@yahoo.se>
2025-11-06 12:09:01 +0100

    Changed date formatting

--- src/components/TrackingData.jsx
+++ src/components/TrackingData.jsx
@@ -4,7 +4,7 @@ export default function TrackingData() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        fetch("https://my.api.mockaroo.com/orders.json?key=e49e6840")
+        fetch("https://file.garden/aFLA3HQurBbOEIEj/packagedata.json")
             .then(response => response.json())
             .then(responseData => setData(responseData))
             .catch(error => console.error("Error fetching data:", error));
@@ -27,18 +27,22 @@ export default function TrackingData() {
                         </tr>
                     </thead>
                     <tbody>
-                        {data.map(item => (
-                            <tr>
-                                <td>{item.parcel_id}</td>
-                                <td>{item.sender}</td>
-                                <td>{item.status}</td>
-                                <td>{item.eta}</td>
-                                <td>{item.verification_required ? "Yes" : "No"}</td>
-                                <td>{item.location_name}</td>
-                                <td>{item.notes}</td>
-                                <td>{item.last_updated}</td>
-                            </tr>
-                        ))}
+                        {data.map(item => {
+                            let eta = new Date(item.eta);
+                            let updated = new Date(item.last_updated);
+                            return (
+                                <tr>
+                                    <td>{item.parcel_id}</td>
+                                    <td>{item.sender}</td>
+                                    <td>{item.status}</td>
+                                    <td>{eta.toLocaleDateString()} <br /> {eta.toLocaleTimeString()}</td>
+                                    <td>{item.verification_required ? "Yes" : "No"}</td>
+                                    <td>{item.location_name}</td>
+                                    <td>{item.notes}</td>
+                                    <td>{updated.toLocaleDateString()} <br /> {updated.toLocaleTimeString()}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             ) : (
